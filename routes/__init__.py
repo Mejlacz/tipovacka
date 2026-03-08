beadea1fe8c8587cbb71d9379b4b7dc5c2f97385
Mejlacz <milmartinec@gmail.com>
2026-03-08 10:18:10 +0100

    Add files via upload

--- routes/__init__.py
+++ routes/__init__.py
@@ -9,9 +9,8 @@ from .admin_users  import register_admin_users
 from .admin_rounds import register_admin_rounds
 from .admin_core   import register_admin_core
 from .admin_import import register_admin_import
-# from .admin_api    import register_admin_api
-# from .pwa          import register_pwa  # PWA vypnuto
 from .errors       import register_errors
+# Odstraněno: admin_api (API fetchery smazány), pwa (vypnuto)
 
 
 def register_all_routes(app):
@@ -23,6 +22,4 @@ def register_all_routes(app):
     register_admin_rounds(app)
     register_admin_core(app)
     register_admin_import(app)
-    # register_admin_api(app)
-    register_pwa(app)
     register_errors(app)
